b07fe8320c8aa5c9f0b6ebc7f73a1ff3a4e7ad89
GabrielMC86 <2393@holbertonschool.com>
2021-02-03 19:58:53 +0000

    Fix code task 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+ * puts - prints a string
+ * @char - contains string 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char s[] = "Programming is like building a multilingual puzzle";
+
+	puts(s); 
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+ * Print a string of characters
+ * printf - print a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
