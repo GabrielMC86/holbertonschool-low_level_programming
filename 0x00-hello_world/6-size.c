358df144072a3e33f60e09675d5e585fe01527e8
GabrielMC86 <2393@holbertonschool.com>
2021-02-04 00:27:23 +0000

    Fix code for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
 * main - function prints the required text and returns 0
 * Return: 0
@@ -10,10 +10,10 @@ int i;
 long int l;
 long long int y;
 float f;
-printf("Size of a char: %zu bytes(s)\n", sizeof(c));
-printf("Size of an int: %zu bytes(s)\n", sizeof(i));
-printf("Size of a long int: %zu bytes(s)\n", sizeof(l));
-printf("Size of a long long int: %zu bytes(s)\n", sizeof(y));
-printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(i));
+printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(y));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
