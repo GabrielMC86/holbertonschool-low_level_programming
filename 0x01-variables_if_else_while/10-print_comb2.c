27b2be9e7d412edfcd3cf0b4ce7e6786d6a3497e
GabrielMC86 <2393@holbertonschool.com>
2021-02-05 18:02:19 +0000

    Fix code for task 10 print double numbers 00-99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,18 +10,18 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		putchar((i/10)+'0');
-		putchar((i%10)+'0');
-		
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+
 		if (i <= 98)
 		{
 		putchar(',');
-		}   
+		}
 		if (i <= 98)
 		{
-		putchar(' '); 
+		putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
-}    
+}
