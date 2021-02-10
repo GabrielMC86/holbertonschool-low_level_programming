0755ef6c4256325d7a5e7c6153a89d54e4c86d64
GabrielMC86 <2393@holbertonschool.com>
2021-02-10 01:53:20 +0000

    Task 4 check if is alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha - confirm if return output is a alphabetic char
+ * @c: variable used
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+return (0);
+}
