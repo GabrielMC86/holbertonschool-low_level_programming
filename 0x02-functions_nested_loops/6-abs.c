4d48846d4d40fc4de8dd516df7085d923373ef97
GabrielMC86 <2393@holbertonschool.com>
2021-02-10 01:40:12 +0000

    Fixing task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
  * _abs - display the absolute value of the integer
- * 
+ * @n: variable
  * Return: 0
  */
 int _abs(int n)
 {
-	if (n >=0)
+	if (n >= 0)
 		return (n);
 
-	else 
+	else
 	{
 		n *= -1;
 		return (n);
-	 
 	}
 }
