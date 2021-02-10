a3127b5202ab18a516232873554fd5b87c77a0b4
GabrielMC86 <2393@holbertonschool.com>
2021-02-10 23:00:35 +0000

    Adding task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print every minute of every hour
+ * int: type  var used
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0 ; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
