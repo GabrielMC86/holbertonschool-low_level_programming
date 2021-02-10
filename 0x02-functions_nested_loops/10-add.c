56e9ddbff3e1f66f9c62c628c1b5382ccc273f83
GabrielMC86 <2393@holbertonschool.com>
2021-02-10 19:13:11 +0000

    Adding tasks 7 and 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
-#include "holberton.c"
+#include "holberton.h"
 /**
  * add - adds two integer, return result
  * int: variable
  * Return: 0
  */
 
-int add(int, int);
+int add(int a, int b)
 {
+	int sum;
 
-
-
-
-return (0);
+	sum = a+b;
+		
+	return(sum);
 }
