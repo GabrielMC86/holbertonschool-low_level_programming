4c77e6ce21e19324b5d387e06d15832946773e94
GabrielMC86 <2393@holbertonschool.com>
2021-02-11 17:48:13 +0000

    Fixing betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
  * _isupper - checks for upper case letters
+ * @c: variable
  * Return: 1 always success
  */
 int _isupper(int c)
 {
-		
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
