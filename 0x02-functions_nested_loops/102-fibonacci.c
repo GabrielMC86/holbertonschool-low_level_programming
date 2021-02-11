f1c48824c012f7ff79ebc8843bab5cb9f83075c9
GabrielMC86 <2393@holbertonschool.com>
2021-02-11 00:49:10 +0000

    Adding task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,20 +8,19 @@ int main(void)
 {
 	long int f1 = 1;
 	long int f2 = 2;
-	int count = 2; 
+	int count = 2;
 	long int fnext;
 
 	fnext = f1 + f2;
 	printf("1, 2, ");
 
 	while (count < 50)
-	{ 
+	{
 		printf("%li", fnext);
 		if (count != 49)
 		{
 			printf(",");
 		}
-		
 		f1 = f2;
 		f2 = fnext;
 		fnext = f1 + f2;
