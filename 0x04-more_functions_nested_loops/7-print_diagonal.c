5b1c087fa88754e66aae905da98839c6f6f1100f
GabrielMC86 <2393@holbertonschool.com>
2021-02-12 03:49:46 +0000

    Adding task 7,8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal lines
+ * @n: variable
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+		_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
