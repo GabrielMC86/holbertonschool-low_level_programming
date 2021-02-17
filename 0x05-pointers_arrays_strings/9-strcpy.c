6edc737de795fef96cc7a9bcb7fd7ec2acd339dc
GabrielMC86 <2393@holbertonschool.com>
2021-02-17 03:14:06 +0000

    Adding task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copy string 
+ * @src: variable pointer
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src);
+
+
+{
+
+
+
+
+
+
+
+
+return (*d);
+}
