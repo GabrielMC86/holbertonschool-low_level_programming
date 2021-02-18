040ff7e560c298288bb23709e0b239cea25eeb8a
GabrielMC86 <2393@holbertonschool.com>
2021-02-18 20:04:47 +0000

    change dir name

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strncat - concatenates two strings 
+ * @dest: string to be appended
+ * @src: string to modify dest
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+
+
+
+
+
+
+
+}
