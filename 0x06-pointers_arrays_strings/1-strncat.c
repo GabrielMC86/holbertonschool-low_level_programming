ac7ae0052da841968a1dc79f9cdbc52256af8b07
GabrielMC86 <2393@holbertonschool.com>
2021-02-18 23:27:53 +0000

    Adding task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,29 @@
 #include "holberton.h"
 /**
- * _strncat - concatenates two strings 
- * @dest: string to be appended
- * @src: string to modify dest
- * Return: char
+ * _strncpy - concatenates two strings
+ * @dest: string to be concatenated
+ * @src: string that modifies dest
+ * @n: paramenter checking len
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int len;
+	int dest_len = _strlen(dest);
 
+	for (len = 0; len < n && src[len] != '\0'; len++)
+		dest[dest_len + len] = src[len];
+	
+	dest[dest_len + len] = '\0';
 
+	return (dest);
+}
+int _strlen(char *s)
+{
+	int len = 0;
 
-
-
-
-
-
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
