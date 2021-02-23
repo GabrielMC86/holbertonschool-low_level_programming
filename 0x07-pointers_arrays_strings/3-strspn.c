0e31eceb4ac3f53ad095d6901cbf0ddc126a397f
GabrielMC86 <2393@holbertonschool.com>
2021-02-23 02:42:49 +0000

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h" 
+/**
+ * _strspn - get lenght of prefix string
+ * @s: pointer varaible
+ * @accept: variable 
+ * Return: number of bytes
+ */
+ unsigned int _strspn(char *s, char *accept)
+{
+	
+	int abc;
+	int xyz;
+
+
+	for (abc = 0; s[abc] != '\0'; abc++)
+	{
+		for (xyz = 0; accept[xyz] != s[abc]; xyz++)
+		{
+			if (accept[xyz] == '\0')
+			{
+				return (abc);
+
+			}
+		}
+	
+	}
+return (abc);
+}
