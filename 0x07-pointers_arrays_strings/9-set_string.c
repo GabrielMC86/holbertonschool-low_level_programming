223dfa8653a6bf0e8003eecbb8a2419b88779dc7
GabrielMC86 <2393@holbertonschool.com>
2021-02-24 03:58:24 +0000

    Set double pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h" 
+/**
+ * set_string - set pointer to to pointer
+ * @s: double pointer
+ * @to: pointer to set
+ * Return: Void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
