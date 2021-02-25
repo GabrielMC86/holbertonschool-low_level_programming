de33879b880ba0498548766693f89cb52b3c07f0
GabrielMC86 <2393@holbertonschool.com>
2021-02-25 01:18:38 +0000

    Print string reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
-#include "holberton.h" 
+#include "holberton.h"
 /**
- * _print_rev_recursion - print a string in reverse 
+ * _print_rev_recursion - print a string in reverse
  * @s: pointer variable to string
  * Return: Void
  */
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 	if (s[0] == '\0')
 	{
 	return;
-	}	
+	}
 	_print_rev_recursion(s + 1);
 	_putchar(s[0]);
 }
