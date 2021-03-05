0e6ecb07dc4ee3cd4725c85e1ab4f1cc82ed3ff8
GabrielMC86 <2393@holbertonschool.com>
2021-03-05 02:10:34 +0000

    Adding two string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,14 +10,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_str;
-	unsigned int lenght = _strlen(s1);
-	unsigned int lenght2 = _strlen(s2);
+	unsigned int lenght;
+	unsigned int lenght2;
 	unsigned int ctr;
 
-	if (s1 == NULL)
-		s1 = 0;
-	if (s2 == NULL)
-		s2 = 0;
+	if (s1 == 0)
+		s1 = NULL;
+	else
+		for (lenght = 0; s1[lenght] != '\0'; lenght++)
+			;
+
+	if (s2 == 0)
+		s2 = NULL;
+	else
+		for (lenght2 = 0; s2[lenght2] != '\0'; lenght2++)
+			;
 
 	if (lenght2 > n)
 		lenght2 = n;
@@ -35,19 +42,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	new_str[lenght + lenght2] = '\0';
 	return (new_str);
-}
-/**
- * _strlen - gives out lenght of string
- * @s: pointer variable
- * Return: lenght of string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-return (len);
 }
