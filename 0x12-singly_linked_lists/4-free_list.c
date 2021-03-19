8c6cb60699f5709654fa37414c7e588e37f102b2
GabrielMC86 <2393@holbertonschool.com>
2021-03-19 04:18:27 +0000

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,10 @@
 
 void free_list(list_t *head)
 {
-        while (head != NULL)
-        {
-                free(head->str);
-                free(head);
-                head = head->next;
-        }
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
 }
