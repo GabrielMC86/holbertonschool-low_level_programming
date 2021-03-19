c4f8be678c4db7ab9f175823a5f6c02d581c43dc
GabrielMC86 <2393@holbertonschool.com>
2021-03-19 04:11:25 +0000

    Hot fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H
-#define HOLBERTON_H 
+#define HOLBERTON_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,13 +14,13 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h); /*Prints all elements in list*/
-size_t list_len(const list_t *h); /*Returns number of elements in a linked list*/
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
