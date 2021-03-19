66a08a8797edff48e0e4677b76cc11b6a3123fd1
GabrielMC86 <2393@holbertonschool.com>
2021-03-19 02:08:11 +0000

    Printing list with single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h" 
+/**
+ * print_list - Prints all the elements of a list
+ * @h: pointer to structure
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int idx;
+
+	
+	idx = 0;
+	while(h != NULL)
+	{
+		if(h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		idx++;
+		h = h->next;
+	}
+return(idx);
+}
