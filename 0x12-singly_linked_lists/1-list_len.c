2cccc829ae27606b85c76e0d1a83d838bbeb35b9
GabrielMC86 <2393@holbertonschool.com>
2021-03-19 04:27:08 +0000

    Returns numbers of elements linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - function that returns number of elements in a linked list
+ * @h: pointer to struct
+ * Return: returns number of elemrnts linked
+ */
+size_t list_len(const list_t *h)
+{
+	int idx;
+
+	while (h != NULL)
+	{
+		idx++;
+		h = h->next;
+	}
+return (idx);
+}
