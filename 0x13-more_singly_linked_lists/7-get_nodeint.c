7dc8804b087ddcb15dedc557c0094987d53e3004
GabrielMC86 <2393@holbertonschool.com>
2021-03-23 23:18:08 +0000

    Fixing betty for task 5 && 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - functions that returns specific element at list
+ * @head: pointer at index
+ * @index: elements at list
+ * Return: 0 on success
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+
+	count = 0;
+	while (count < index && head != NULL)
+	{
+		count++;
+		head = head->next;
+
+		if (count == index)
+		{
+			return (head);
+		}
+	}
+return (0);
+}
