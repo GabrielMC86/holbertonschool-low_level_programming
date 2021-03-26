6ccfc6adbea8f6ec2769a43dbc769d9d27e849ae
GabrielMC86 <2393@holbertonschool.com>
2021-03-26 04:01:19 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+void print_binary(unsigned long int n)
+{
+        unsigned long int num, maske = 1;
+        int largo = 0;
+
+        num = n;
+        while (num > 0)
+        {
+                largo++;
+                num >>= 1;
+        }
+
+        largo -= 1;
+
+        if (largo > 0)
+        {
+                maske = maske << largo;
+        }
+
+        while (maske > 0)
+        {
+                if (n & maske)
+                {
+                        _putchar('1');
+                }
+                else
+                {
+                        _putchar('0');
+                }
+                maske >>= 1;
+        }
+}
+
