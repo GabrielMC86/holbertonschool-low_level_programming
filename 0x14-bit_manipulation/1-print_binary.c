aede82d9dbc82518ad063967d5169a552e5096e7
GabrielMC86 <2393@holbertonschool.com>
2021-03-26 04:13:41 +0000

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,39 @@
+#include "holberton.h"
+/**
+ * print_binary - Prints binary numbers
+ * @n: numbers to be printed in binary
+ * Return: binary nuber
+ */
 void print_binary(unsigned long int n)
 {
-        unsigned long int num, maske = 1;
-        int largo = 0;
+	unsigned long int num, maske = 1;
+	int largo = 0;
 
-        num = n;
-        while (num > 0)
-        {
-                largo++;
-                num >>= 1;
-        }
+	num = n;
+	while (num > 0)
+	{
+		largo++;
+		num >>= 1;
+	}
 
-        largo -= 1;
+	largo -= 1;
 
-        if (largo > 0)
-        {
-                maske = maske << largo;
-        }
+	if (largo > 0)
+	{
+		maske = maske << largo;
+	}
 
-        while (maske > 0)
-        {
-                if (n & maske)
-                {
-                        _putchar('1');
-                }
-                else
-                {
-                        _putchar('0');
-                }
-                maske >>= 1;
-        }
+	while (maske > 0)
+	{
+		if (n & maske)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		maske >>= 1;
+	}
 }
 
