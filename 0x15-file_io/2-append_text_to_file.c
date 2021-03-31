d6791a7e58db847c95ea2b31c0e86dc9b8c4fddd
GabrielMC86 <2393@holbertonschool.com>
2021-03-31 20:18:56 +0000

    Append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * create_file - function that creates a file
+ * append_text_to_file - function that creates adds a line to file
  * @filename: pointer variable to file
  * @text_content: a NULL terminated string dont add to file
  * Return: 1 on sucess, -1 on fail
@@ -9,14 +9,15 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fdescript, mask_write, count;
 
-	fdescript = open(filename, O_WRONLY | O_APPEND);
+	fdescript = (open(filename, O_WRONLY | O_APPEND));
 
 	if (fdescript == -1)
 		return (-1);
 
-	if (*text_content == -1)
+	if (text_content == NULL)
+	{
 		return (1);
-
+	}
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
 
